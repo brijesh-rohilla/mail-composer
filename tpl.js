04fc60cff08c5fb7afc712d9001d964acd88f402
Brijesh Rohilla <iambrijeshrohilla@gmail.com>
2026-05-08 18:54:30 +0530

    fix: message updated

---
+++ tpl.js
@@ -0,0 +1,25 @@
+window.TPL = {
+  subject: `Application for {position} Position at {company}`,
+  body: `Hi {hrName},
+Greetings !!
+
+I hope you're doing well. My name is <strong>Brijesh</strong>, and I am currently working as a <strong>Software Developer (MERN) at Softobiz Technologies</strong>. I am genuinely excited about the opportunity to join the team at {__BOLD__company} as a {__BOLD__position} Developer. I came across this opening on LinkedIn, and I would be truly grateful for your assistance with a referral.
+
+Notice period:-  Immediate Joiner (15 days or earlier)
+LinkedIn Profile:- https://www.linkedin.com/in/brijesh-rohilla
+
+I have <strong>4+ years of experience in {position} Development</strong>. I would be grateful if you could look at my resume once to gauge my fit. I've attached my resume for your reference. Thank you for your time. It would be great if I could get one chance. <strong>I look forward to your response</strong>.
+
+Thanks & regards
+Brijesh
+`,
+  li: `Hi {hrName},
+Happy to connect with you. Actually, I really want to join your organization as {position} Developer. I already shared my resume at your email.
+
+Experience:- 4+ years
+Notice period:-  Immediate Joiner (15 days or earlier)
+
+If anything is needed from my side please ask. I am also sharing my resume here. I would be happy if you give me some priority.
+
+Thank you 🙂`,
+};
