3c589653e6e5ce3a6c8656d172013d67b48f16c8
Brijesh Rohilla <iambrijeshrohilla@gmail.com>
2026-05-11 11:36:11 +0530

    fix: copy message at clipboard

--- tpl.js
+++ tpl.js
@@ -1,14 +1,14 @@
 window.TPL = {
-  subject: `Application for {position} Position at {company}`,
+  subject: `Application for {position} role at {company}`,
   body: `Hi {hrName},
-Greetings !!
+Greetings!!
 
-I hope you're doing well. My name is <strong>Brijesh</strong>, and I am currently working as a <strong>Software Developer (MERN) at Softobiz Technologies</strong>. I am genuinely excited about the opportunity to join the team at {__BOLD__company} as a {__BOLD__position} Developer. I came across this opening on LinkedIn, and I would be truly grateful for your assistance with a referral.
+I hope you're doing well. My name is <strong>Brijesh</strong>, and I am currently working as a <strong>Software Developer (MERN)</strong> at <strong>Softobiz Technologies</strong>. I am genuinely excited about the opportunity to join the team at {__BOLD__company} as a {__BOLD__position}. I came across this opening on LinkedIn, and I would be truly grateful for your assistance with a referral.
 
 Notice period:-  Immediate Joiner (15 days or earlier)
-LinkedIn Profile:- https://www.linkedin.com/in/brijesh-rohilla
+LinkedIn Profile:- <a href="https://www.linkedin.com/in/brijesh-rohilla" target="_blank">https://www.linkedin.com/in/brijesh-rohilla</a>
 
-I have <strong>4+ years of experience in {position} Development</strong>. I would be grateful if you could look at my resume once to gauge my fit. I've attached my resume for your reference. Thank you for your time. It would be great if I could get one chance. <strong>I look forward to your response</strong>.
+I have <strong>4+ years</strong> of experience in <strong>{position} Development</strong>. I would be grateful if you could look at my resume once to gauge my fit. I've attached my resume for your reference. Thank you for your time. It would be great if I could get one chance. <strong>I look forward to your response</strong>.
 
 Thanks & regards
 Brijesh
